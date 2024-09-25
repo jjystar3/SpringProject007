e4f4662c5331eacb6724c6c5585f1912e657bcd7
JuYoung <jjystar3@gmail.com>
2024-09-25 14:54:31 +0900

    first commit

---
+++ src/main/java/com/example/demo/Project007Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project007Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project007Application.class, args);
+	}
+
+}
