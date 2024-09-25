7698317023955cd08f90f146cd732cc27555c9d8
JuYoung <jjystar3@gmail.com>
2024-09-25 16:27:16 +0900

    자동으로 처리되는 날짜 설정

---
+++ src/test/java/com/example/demo/repository/BoardRepositoryTest.java
@@ -0,0 +1,62 @@
+package com.example.demo.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.demo.entity.Board;
+
+// 스프링 컨테이너 환경을 임시로 불러오는 기능
+@SpringBootTest
+public class BoardRepositoryTest {
+
+	// 컨테이너 안에 있는 리파지토리 빈 주입
+	@Autowired
+	BoardRepository repository;
+
+	@Test // 단위테스트
+	void 리파지토리빈_확인() {
+		System.out.println("repository:" + repository);
+	}
+	
+	@Test
+	void 게시물등록() {
+
+		//시간은 입력할 필요없음
+		Board board = Board.builder()
+						.title("1번글").content("내용입니다").writer("둘리")
+						.build();
+		repository.save(board);
+	}
+//
+//	@Test
+//	void 게시물목록조회() {
+//		List<Board> list = repository.findAll();
+//		for(Board board : list) {
+//			System.out.println(board);
+//		}
+//	}
+//	
+//	@Test
+//	void 게시물단건조회() {
+//		Optional<Board> result = repository.findById(1);
+//		if(result.isPresent()) {
+//			Board board = result.get();
+//			System.out.println(board);
+//		}
+//	}
+//
+//	@Test
+//	void 게시물수정() {
+//		Optional<Board> result = repository.findById(1);
+//		Board board = result.get();
+//		board.setContent("내용이수정되었습니다~");
+//		repository.save(board);
+//	}
+//	
+//	@Test
+//	void 게시물삭제() {
+//		repository.deleteById(1);
+//	}
+//	
+}
